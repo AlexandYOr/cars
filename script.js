8ca6ea679a14af236c8cf2f11e58404e54ac4e08
AlexandYOr <avo01121997@yandex.ru>
2023-10-18 15:33:03 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const select = document.querySelector('#cars')
+const selectedCar = select.querySelectorAll('option')
+const output = document.querySelector('#output');
+
+select.addEventListener('change', () => {
+    fetch('cars.json')
+        .then((res) => res.json())
+        .then((res) => {
+            selectedCar.forEach((car) => {
+                if (car.selected) {
+                    const {brand, model, price} = res.cars[car.index + 1] 
+                    console.log(brand)
+                    output.innerHTML = (`Тачка ${brand} ${model} <br> Цена: ${price}$`)
+                }
+            })
+        })
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err))
+})
+
+
